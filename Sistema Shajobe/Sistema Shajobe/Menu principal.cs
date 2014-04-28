35244099828998bf38e043e3acf824f92157d059
José Ricardo <josericardo.ac@hotmail.com>
2014-04-28 00:29:39 -0500

    Cargando elementos de diseño
    Se cargaron imagenes al sistema y se crearon dos formas en las cuales se
    va a empezar a trabajar

---
+++ Sistema Shajobe/Sistema Shajobe/Menu principal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sistema_Shajobe
+{
+    public partial class Menu_principal : Form
+    {
+        public Menu_principal()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Sistema Shajobe/Sistema Shajobe/Tipo pieza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sistema_Shajobe
+{
+    public partial class Tipo_pieza : Form
+    {
+        public Tipo_pieza()
+        {
+            InitializeComponent();
+        }
+    }
+}
